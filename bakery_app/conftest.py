4b90526ec1156b5d45d476b43ea087e2727f8c2a
Joseph Hanna <joseph.com.ec@gmail.com>
2024-01-15 16:53:57 +0000

    Initialize project structure

---
+++ bakery_app/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bakery_app.users.models import User
+from bakery_app.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
