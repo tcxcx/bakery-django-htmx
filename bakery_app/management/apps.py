095cd901d30f2bf49ccb5168f98ea69bf2ce4cfa
Joseph Hanna <joseph.com.ec@gmail.com>
2024-01-30 17:30:25 -0500

    Fix appname error

--- bakery_app/management/apps.py
+++ bakery_app/management/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AManagementConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "management"
+    name = "bakery_app.management"
